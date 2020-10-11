241381af6032a56d28f9474f460447eb9e8efb71
EC2 Default User <ec2-user@ip-172-31-82-12.ec2.internal>
2020-10-11 17:55:54 +0000

    Change variable names related to messages

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import flask_sqlalchemy
 import flask_socketio
 import models 
 
-ADDRESSES_RECEIVED_CHANNEL = 'message received'
+MESSAGES_RECEIVED_CHANNEL = 'message received'
 
 app = flask.Flask(__name__)
 
@@ -30,13 +30,12 @@ db = flask_sqlalchemy.SQLAlchemy(app)
 db.init_app(app)
 db.app = app
 
-
 db.create_all()
 db.session.commit()
 
-def emit_all_addresses(channel):
-    all_addresses = [db_address.address for db_address in db.session.query(models.Usps).all()]
-    socketio.emit(channel, {'allMessages': all_addresses})
+def emit_all_messages(channel):
+    all_messages = [db_message.address for db_message in db.session.query(models.Usps).all()]
+    socketio.emit(channel, {'allMessages': all_messages})
 
 @socketio.on('connect')
 def on_connect():
@@ -45,7 +44,7 @@ def on_connect():
         'test': 'Connected'
     })
     
-    emit_all_addresses(ADDRESSES_RECEIVED_CHANNEL)
+    emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
 
 @socketio.on('disconnect')
 def on_disconnect():
@@ -58,11 +57,11 @@ def on_new_address(data):
     db.session.add(models.Usps(data['message']));
     db.session.commit();
     
-    emit_all_addresses(ADDRESSES_RECEIVED_CHANNEL)
+    emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
 
 @app.route('/')
 def index():
-    emit_all_addresses(ADDRESSES_RECEIVED_CHANNEL)
+    emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
 
     return flask.render_template("index.html")
 
