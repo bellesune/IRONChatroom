97900ca0802156709772d6727af3802c6259ae58
EC2 Default User <ec2-user@ip-172-31-82-12.ec2.internal>
2020-10-13 07:38:48 +0000

    Add style to messages, add pink and white background

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Input } from './Input';
 import { Socket } from './Socket';
+// import bg from './bg.jpg';
 
 const bodyStyle = {
     textAlign: 'center',
@@ -25,35 +26,60 @@ const messagesGrid = {
     marginLeft: 'auto',
     marginRight: 'auto',
     padding: '28px',
-    
+    // backgroundImage: {bg},
+    // backgroundPosition: 'center',
+    // backgroundSize: 'cover',
 };
+
+const messageCard = {
+    borderRadius: '15px',
+    padding: '10px',
+    color: 'black',
+    backgroundColor: 'white',
+    marginTop: '10px',
+    margin: '10px',
+};
+
 
 export function Content() {
     const [messages, setMessage] = React.useState([]);
-    const [count, setCount] = React.useState([]);
+    const [count, setCount] = React.useState(0);
+    const [bot, setBot] = React.useState([]);
     
-    function getNewAddresses() {
+    function getNewMessages() {
         React.useEffect(() => {
             Socket.on('message received', (data) => {
                 console.log(`Received message from server: ${data['allMessages']}`)
                 setMessage(data['allMessages']);
                 setCount(data['user_count']);
+                // console.log("THIS IS ",data['allMessages'].slice(0,7))
+                
+                // for (let i=0; i<data['allMessages'].length; i++){
+                //     if (data['allMessages'][i].slice(0,7) === "IronBot"){
+                        // setBot(data['allMessages'][i])
+                        // console.log(data['allMessages'][i])
+                        
+                //     }    
+                // }
+                
             });
         });
     }
     
-    getNewAddresses();
+    getNewMessages();
 
     return (
         <div style={bodyStyle}>
             <h1 style={titleStyle}>IRON Chatroom</h1>
             <h4 id="subtitle">Instant Real-time Online Navigator</h4>
             
-            <h2 style={activeUserStyle}>Active users: {count}</h2>
+            <h3 style={activeUserStyle}>Active users: {count}</h3>
                 <div style={messagesGrid}>
                     {
                     messages.map((message, index) => 
-                    <div key={index}>{message}</div>)
+                    <div style={messageCard} key={index}>
+                        {message}
+                    </div>)
                     }
                 </div>
             <Input/>
