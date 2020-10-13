8bdcf0435630373253822c4f19b54bf5f7023b76
EC2 Default User <ec2-user@ip-172-31-82-12.ec2.internal>
2020-10-13 05:39:01 +0000

    Add border and color on messages, align the card in center

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -2,6 +2,32 @@ import * as React from 'react';
 import { Input } from './Input';
 import { Socket } from './Socket';
 
+const bodyStyle = {
+    textAlign: 'center',
+};
+
+const activeUserStyle = {
+    fontFamily: 'Roboto',
+    color: 'thistle',
+};
+
+const titleStyle = {
+    fontFamily: 'Serif',
+    color: 'rgb(212, 111, 212)'
+};
+
+const messagesGrid = {
+    textAlign: 'left',
+    backgroundColor: 'papayaWhip',
+    opacity: '0.83',
+    borderRadius: '15px',
+    width: '65%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    padding: '28px',
+    
+};
+
 export function Content() {
     const [messages, setMessage] = React.useState([]);
     const [count, setCount] = React.useState([]);
@@ -11,7 +37,7 @@ export function Content() {
             Socket.on('message received', (data) => {
                 console.log(`Received message from server: ${data['allMessages']}`)
                 setMessage(data['allMessages']);
-                setCount(data['user_count'])
+                setCount(data['user_count']);
             });
         });
     }
@@ -19,15 +45,17 @@ export function Content() {
     getNewAddresses();
 
     return (
-        <div>
-            <h2>Active users: {count}</h2>
-            <h1>Your messages:</h1>
-                <ol>
+        <div style={bodyStyle}>
+            <h1 style={titleStyle}>IRON Chatroom</h1>
+            <h4 id="subtitle">Instant Real-time Online Navigator</h4>
+            
+            <h2 style={activeUserStyle}>Active users: {count}</h2>
+                <div style={messagesGrid}>
                     {
-                    messages.map((address, index) => 
-                    <li key={index}>{address}</li>)
+                    messages.map((message, index) => 
+                    <div key={index}>{message}</div>)
                     }
-                </ol>
+                </div>
             <Input/>
         </div>
     );
