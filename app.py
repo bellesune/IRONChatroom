0f0800aad0c9ca32681d32751d676689dd2732fd
EC2 Default User <ec2-user@ip-172-31-82-12.ec2.internal>
2020-10-27 04:35:48 +0000

    Sanitize app.py

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
+""" Import flask, socket, and requests """
+import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-from bot import Chatbot
-from flask import request
-from flask_socketio import join_room, leave_room
 from urlextract import URLExtract
-from urllib.parse import urlparse
-import os
 import flask
 import flask_sqlalchemy
 import flask_socketio
-import random
 import requests
+import models
+from bot import Chatbot
 
-app = flask.Flask(__name__)
+APP = flask.Flask(__name__)
 
-db = flask_sqlalchemy.SQLAlchemy(app)
+DB = flask_sqlalchemy.SQLAlchemy(APP)
+
+
 def init_db(app):
-    db.init_app(app)
-    db.app = app
-    db.create_all() 
-    db.session.commit() 
+    """ Initialize database """
+    DB.init_app(app)
+    DB.app = app
+    DB.create_all()
+    DB.session.commit()
 
-import models 
 
-MESSAGES_RECEIVED_CHANNEL = 'message received'
-USERS_UPDATED_CHANNEL = 'users updated'
+MESSAGES_RECEIVED_CHANNEL = "message received"
+USERS_UPDATED_CHANNEL = "users updated"
 
 USERNAME = ""
 TYPE = ""
@@ -32,134 +32,147 @@ IMAGE = ""
 AUTH = ""
 USER_COUNT = 0
 USER_LIST = []
-isLoggedIn = False
+ISLOGGEDIN = False
 
-socketio = flask_socketio.SocketIO(app)
-socketio.init_app(app, cors_allowed_origins="*")
+SOCKETIO = flask_socketio.SocketIO(APP)
+SOCKETIO.init_app(APP, cors_allowed_origins="*")
 
-dotenv_path = join(dirname(__file__), 'sql.env')
-load_dotenv(dotenv_path)
+DOTENV_PATH = join(dirname(__file__), "sql.env")
+load_dotenv(DOTENV_PATH)
 
-database_uri = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+DATABASE_URI = os.environ["DATABASE_URL"]
+APP.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
 
-
-#db = flask_sqlalchemy.SQLAlchemy(app)
 
 def emit_all_messages(channel):
-    all_users = [user.user for user in db.session.query(models.Chatroom).all()]
-    all_auth = [user.auth_type for user in db.session.query(models.Chatroom).all()]
-    all_image = [user.image for user in db.session.query(models.Chatroom).all()]
-    all_messages = [user.message for user in db.session.query(models.Chatroom).all()]
-    all_type = [user.role_type for user in db.session.query(models.Chatroom).all()]
-    
-    socketio.emit(channel, {
-        'type': all_type,
-        'allAuth': all_auth,
-        'allUsers': all_users,
-        'allImages': all_image,
-        'allMessages': all_messages,
-        'user_count': USER_COUNT,
-        })
+    """ Get all data from table and send to client """
+    all_users = [user.user for user in DB.session.query(models.Chatroom).all()]
+    all_auth = [user.auth_type for user in DB.session.query(models.Chatroom).all()]
+    all_image = [user.image for user in DB.session.query(models.Chatroom).all()]
+    all_messages = [user.message for user in DB.session.query(models.Chatroom).all()]
+    all_type = [user.role_type for user in DB.session.query(models.Chatroom).all()]
+
+    SOCKETIO.emit(
+        channel,
+        {
+            "type": all_type,
+            "allAuth": all_auth,
+            "allUsers": all_users,
+            "allImages": all_image,
+            "allMessages": all_messages,
+            "user_count": USER_COUNT,
+        },
+    )
+
 
 def push_new_user_to_db(name, email, auth_type, image_url):
-    db.session.add(models.AuthUser(name, email, auth_type, image_url));
-    db.session.commit();
-    
+    """ Add user details to db """
+    DB.session.add(models.AuthUser(name, email, auth_type, image_url))
+    DB.session.commit()
+
+
 def count_user(user, connection):
-    global USER_COUNT 
+    """ Count active users and remove users who disconnected """
+    global USER_COUNT
     global USER_LIST
-    
+
     if user not in USER_LIST and connection == "connected":
         USER_LIST.append(user)
         USER_COUNT += 1
-        
+
     if user in USER_LIST and connection == "disconnected":
         USER_LIST.remove(user)
         USER_COUNT -= 1
-    
-@socketio.on('connect')
+
+
+@SOCKETIO.on("connect")
 def on_connect():
+    """ Listen to all users viewing the page """
     print("Someone is attempting to connect/login")
-    
-    socketio.emit('connected', {
-        'test': 'Connected'
-    })
-    
+
+    SOCKETIO.emit("connected", {"test": "Connected"})
+
     emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
 
-@socketio.on('disconnect')
+
+@SOCKETIO.on("disconnect")
 def on_disconnect():
+    """ Listen to users exiting the page """
     print(USERNAME, "disconnected")
     count_user(USERNAME, "disconnected")
-    
-@socketio.on('new message input')
+
+
+@SOCKETIO.on("new message input")
 def on_new_message(data):
+    """ Receive the messages from user """
     print("Got an event for new message input with data:", data)
-    
-    if isLoggedIn == True:
-        
+
+    if ISLOGGEDIN:
+
         global TYPE
         TYPE = "user"
-        msg = data['message']
-        
+        msg = data["message"]
+
         if msg[:2] == "!!":
-            db.session.add(models.Chatroom(TYPE, AUTH, USERNAME, IMAGE, msg));
-            
+            DB.session.add(models.Chatroom(TYPE, AUTH, USERNAME, IMAGE, msg))
+
             TYPE = "bot"
             bot = Chatbot(msg, USER_LIST)
-            bot_response = bot.getResponse()
-            db.session.add(models.Chatroom(TYPE, AUTH, USERNAME, IMAGE, bot_response));
-            
+            bot_response = bot.get_response()
+            DB.session.add(models.Chatroom(TYPE, AUTH, USERNAME, IMAGE, bot_response))
+
         else:
-            if 'http' in msg:
+            if "http" in msg:
                 extractor = URLExtract()
                 urls = extractor.find_urls(msg)
-                response = requests.get(urls[0]) 
-                type_msg = response.headers['Content-Type']
-                
-                if 'text/html' in type_msg:
+                response = requests.get(urls[0])
+                type_msg = response.headers["Content-Type"]
+
+                if "text/html" in type_msg:
                     TYPE = "html"
-                    
-                if 'image' in type_msg:
+
+                if "image" in type_msg:
                     TYPE = "jpg"
-                
-            db.session.add(models.Chatroom(TYPE, AUTH, USERNAME, IMAGE, msg));
-    
-        db.session.commit();
-    
+
+            DB.session.add(models.Chatroom(TYPE, AUTH, USERNAME, IMAGE, msg))
+
+        DB.session.commit()
+
         emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
-    
-@socketio.on('new google user')
+
+
+@SOCKETIO.on("new google user")
 def on_new_google_user(data):
+    """ Get user's details from Google Auth """
     print("Got an event for new google user input with data:", data)
-    
-    global USERNAME, IMAGE, AUTH, isLoggedIn
-    USERNAME = data['name']
-    email = data['email']
-    IMAGE = data['imageUrl']
-    isLoggedIn = data['successLogin']
+
+    global USERNAME, IMAGE, AUTH, ISLOGGEDIN
+    USERNAME = data["name"]
+    email = data["email"]
+    IMAGE = data["imageUrl"]
+    ISLOGGEDIN = data["successLogin"]
     AUTH = "Google"
-    
-    socketio.emit('login successful', { 
-        'isLoggedIn': isLoggedIn
-    });
-    
+
+    SOCKETIO.emit("login successful", {"ISLOGGEDIN": ISLOGGEDIN})
+
     count_user(USERNAME, "connected")
-    
+
     push_new_user_to_db(USERNAME, email, AUTH, IMAGE)
-    
-@app.route('/')
+
+
+@APP.route("/")
 def index():
+    """ Display all messages in the db when opening the page """
     emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
 
     return flask.render_template("index.html")
 
-if __name__ == '__main__': 
-    init_db(app)
-    socketio.run(
-        app,
-        host=os.getenv('IP', '0.0.0.0'),
-        port=int(os.getenv('PORT', 8080)),
-        debug=True
+
+if __name__ == "__main__":
+    init_db(APP)
+    SOCKETIO.run(
+        APP,
+        host=os.environ("IP", "0.0.0.0"),
+        port=int(os.environ("PORT", 8080)),
+        debug=True,
     )
