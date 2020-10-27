a1f2b9ee494bb99b1875587777cddc083ed6a761
EC2 Default User <ec2-user@ip-172-31-82-12.ec2.internal>
2020-10-27 04:07:15 +0000

    Sanitize and add docstring

--- models.py
+++ models.py
@@ -1,44 +1,50 @@
-# models.py
+""" import sqlalchemy and app """
 import flask_sqlalchemy
-from app import db
 from enum import Enum
+from app import db
+
 
 class Chatroom(db.Model):
+    """ Create columns for chatroom and messages received """
     id = db.Column(db.Integer, primary_key=True)
     role_type = db.Column(db.String(120))
     auth_type = db.Column(db.String(120))
     user = db.Column(db.String(120))
     image = db.Column(db.String(240))
     message = db.Column(db.String(500))
-    
+
     def __init__(self, role_type, auth_type, user, image, msg):
         self.role_type = role_type
         self.auth_type = auth_type
         self.user = user
         self.image = image
         self.message = msg
-        
+
     def __repr__(self):
-        return '<Chatbox message: %s>' % self.message 
+        return "<Chatbox message: %s>" % self.message
+
 
 class AuthUser(db.Model):
+    """ Create columns for Google Auth """
     id = db.Column(db.Integer, primary_key=True)
     auth_type = db.Column(db.String(120))
     image_url = db.Column(db.String(240))
     name = db.Column(db.String(120))
     email = db.Column(db.String(120))
-    
+
     def __init__(self, name, email, auth_type, image_url):
         # assert type(auth_type) is AuthUserType
         self.name = name
         self.email = email
-        self.auth_type = auth_type #auth_type.value
+        self.auth_type = auth_type  # auth_type.value
         self.image_url = image_url
-        
+
     def __repr__(self):
         return "<User name: {}\ntype: {}".format(self.name, self.auth_type)
 
+
 class AuthUserType(Enum):
+    """ Enumerate the type of authentication used """
     GOOGLE = "google"
     FACEBOOK = "facebook"
     PASSWORD = "password"
